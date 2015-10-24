0f43d7a15de36555cb3eb7fd3159e6173792f5a4
risan <risanbagja@gmail.com>
2015-10-24 09:04:48 +0200

    Remove console output at footer controller.

--- assets/js/controllers/FooterCtrl.js
+++ assets/js/controllers/FooterCtrl.js
@@ -2,5 +2,4 @@
 
 angular.module('adminLTEApp.Controllers').controller('FooterCtrl', ['$scope', 'META_DATA', function($scope, META_DATA) {
   $scope.meta = META_DATA;
-  console.log($scope.meta);
 }]);
